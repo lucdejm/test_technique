f1246824e57a1c8596026abcff1942230f5f1dd0
lucdejm <luc.dejm@gmail.com>
2018-08-26 09:11:05 +0200

    fetch random movie

--- public/js/randommovie.js
+++ public/js/randommovie.js
@@ -4,27 +4,8 @@ const movieCards = 8;
 
 /* fetch functions */
 
-/*
-fetch(url)
-    .then((resp) => resp.json())
-    .then(function(data) {
-        let movieCard = data;
-        return movieCard.map(function(movie) {
-        let li = createNode('li'),
-            img = createNode('img'),
-            span = createNode('span');
-        img.src = movie.movie_cover; 
-        span.innerHTML = `De ${movie.director}`;
-        append(li, img);
-        append(li, span);
-        append(ul, li);
-        })
-    })
-    .catch(error => console.log("There is an error", error))
-*/
-
 for(i=0; i<movieCards; i++) {
-    let id = randomId(200); // la variable doit correspondre au max id en base
+    let id = randomId(50); // la variable doit correspondre au max id en base
     console.log(id);
     let urlMovie = "http://localhost:3000/movies/"+id;
     fetch(urlMovie)
@@ -58,33 +39,5 @@ function append(parent, el) {
 }
 
 function randomId(numberOfMovies) {
-    return Math.floor(Math.random() * numberOfMovies);
+    return Math.floor(Math.random() * numberOfMovies) + 1;
 }
-
-
-
-
-
-/*
-for(i=0; i<numberOfMovie; i++) {
-    let id = randomId(200); // la variable doit correspondre au max id en base
-    let url = "http://localhost:3000/randommovies"+"/"+id;
-    fetch(url)
-        .then((resp) => resp.json())
-        .then(function(data) {
-            let movieCard = data;
-            console.log(data);
-            return movieCard.map(function(movie) {
-            let li = createNode('li'),
-                img = createNode('img'),
-                span = createNode('span');
-            img.src = movie.movie_cover; 
-            span.innerHTML = `De ${movie.director}`;
-            append(li, img);
-            append(li, span);
-            append(ul, li);
-            })
-        })
-        .catch(error => console.log("There is an error", error))
-}
-*/
